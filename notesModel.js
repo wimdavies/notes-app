5e7b290f64760ccc0d2d895323adae68b1b5fcd7
wimdavies <william.johnston.davies@gmail.com>
2023-05-30 14:41:22 +0100

    testdrove NotesModel class

---
+++ notesModel.js
@@ -0,0 +1,20 @@
+
+class NotesModel {
+  constructor() {
+      this.notes = [];
+  };
+
+  getNotes() {
+      return this.notes;
+  };
+
+  addNotes(note) {
+      this.notes.push(note);
+  }
+
+  reset() {
+      this.notes = [];
+  }
+}
+
+module.exports = NotesModel
---
+++ notesModel.test.js
@@ -0,0 +1,28 @@
+const NotesModel = require('./notesModel');
+
+describe("NotesModel", () => {
+    it("#getNotes returns a empty array at first", () => {
+        const notes = new NotesModel;
+
+        expect(notes.getNotes()).toEqual([]);
+    })
+
+    it('#addNotes adds new notes to the array', () => {
+        const notes = new NotesModel;
+
+        notes.addNotes('Go to the shops');
+        notes.addNotes('Buy milk');
+        
+        expect(notes.getNotes()).toEqual(['Go to the shops', 'Buy milk']);
+    })
+
+    it('#reset deletes all notes from the array', () => {
+        const notes = new NotesModel;
+
+        notes.addNotes('Go to the shops');
+        notes.addNotes('Buy milk');
+        notes.reset();
+
+        expect(notes.getNotes()).toEqual([]);
+    })
+})
